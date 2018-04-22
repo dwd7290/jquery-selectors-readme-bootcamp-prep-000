0bb3b9019180463462de5b1138c7113bf8152be6
David <dwd7290@yahoo.com>
2018-04-22 08:11:27 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -12,6 +12,10 @@ function ninjaBabySelector() {
   return $('#baby-ninja');
 }
 
+function divSelector() {
+  return $('.pics');
+}
+
 function firstListItem() {
   return $("ul#pic-list li:first-child");
 }
